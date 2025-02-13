1f194ae1a0342d2009e21cebb2aabecd334a4ba0
thanhben03 <nben19733@gmail.com>
2025-02-13 23:25:20 +0700

    add domain session

--- config/session.php
+++ config/session.php
@@ -128,7 +128,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_session'
     ),
 
     /*
@@ -197,5 +197,5 @@ return [
     */
 
     'same_site' => 'lax',
-
+    'domain' => '.domain.com',
 ];
